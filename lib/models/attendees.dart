8f8007fd45ab0a64d14fe7b3edbfe9bce6043009
William <williammar954@hotmail.com>
2024-06-04 08:31:12 -0500

    feat: add attendees model and attendees files

---
+++ lib/models/attendees.dart
@@ -0,0 +1,22 @@
+class Attendees{
+  int id;
+  String userName;
+  String name;
+  String email;
+
+  Attendees({
+    required this.id,
+    required this.userName,
+    required this.name,
+    required this.email
+  });
+
+  static Attendees fromJson(Map<String, dynamic> json){
+    return Attendees(
+      id: json["id"] as int,
+      userName: json["userName"] as String,
+      name: json["name"] as String,
+      email: json["email"] as String
+    );
+  }
+}
---
+++ lib/services/attendees_list_service.dart
@@ -0,0 +1,9 @@
+import 'package:big_fun_app/models/attendees.dart';
+import 'package:http/http.dart' as http;
+
+class AttendeesListService{
+  static Future<List<Attendees>> getAttendeesByEvent(int eventId) async{
+    http.Response rspta = http.get(U)
+    return [];
+  }
+}
